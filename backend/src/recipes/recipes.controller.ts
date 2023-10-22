9cb9bde1f26a6e76f3754bc7d469f16a5d2fdbb9
Suenodk <suenodekort@gmail.com>
2023-10-22 13:43:25 +0200

    Prisma added, gitignore hoisted up

--- backend/src/recipes/recipes.controller.ts
+++ backend/src/recipes/recipes.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { RecipeDto } from 'src/dto/recipeDto';
+import { RecipeDto } from 'src/dto/RecipeDto';
+import { PrismaService } from 'src/prisma.service';
 
 @Controller('recipes')
 export class RecipesController {
+  constructor(private prisma: PrismaService) {}
   @Get()
-  getRecipes(): RecipeDto {
+  async getRecipes(): Promise<RecipeDto> {
+    const recipes = await this.prisma.recipe.findMany();
+    console.log(recipes);
     return { name: 'Food!' };
   }
 }
