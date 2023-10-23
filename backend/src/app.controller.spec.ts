3b1a4c5cae7f03c092d0f872e72dcd86a23f332d
Suenodk <suenodekort@gmail.com>
2023-10-23 08:12:21 +0200

    fixed backend tests

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecipesService } from './recipes/recipes.service';
+import { PrismaService } from './prisma.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +10,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, RecipesService, PrismaService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
