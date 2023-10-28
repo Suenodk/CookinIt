92935c94fdf1fe1590d7bfc4bbb990599a3fc68f
Suenodk <suenodekort@gmail.com>
2023-10-28 06:42:15 +0200

    updated cors, multiple domains allowed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: [...(process.env.FRONTEND_URL?.split(' ') || 'Origin')],
   });
   await app.listen(process.env.PORT || 8080);
 }
