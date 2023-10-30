abcfae224f0d958eb01b6cedd27711c3da5acdb5
Suenodk <suenodekort@gmail.com>
2023-10-30 18:04:59 +0100

    updated models

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,4 +1,5 @@
 export { CreateRecipeDto } from "./dto/CreateRecipeDto";
+export { UpdateRecipeDto } from "./dto/UpdateRecipeDto";
 export { Ingredient } from "./models/Ingredient";
 export { Recipe } from "./models/Recipe";
 export { RecipeStep } from "./models/RecipeStep";
